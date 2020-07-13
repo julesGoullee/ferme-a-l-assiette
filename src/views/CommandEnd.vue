ecf3ae3d2e930bd8cb42b4aa5bd016f9f9acda9d
julesGoullee <julesgoullee@gmail.com>
2020-07-14 04:58:27 +0700

    mobile

--- src/views/CommandEnd.vue
+++ src/views/CommandEnd.vue
@@ -3,7 +3,7 @@
     <b-row  align-v="center" style="margin: 10%">
       <b-col cols="12">
         <b-icon icon="check2-circle" color="green" font-scale="4" style="vertical-align: middle;"></b-icon>
-        <h1 style="display: inline; font-size: 30px; margin-left: 15px; vertical-align: middle;">Commande confirmée!</h1>
+        <h1 style="display: inline; font-size: 27px; margin-left: 15px; vertical-align: middle;">Commande confirmée!</h1>
       </b-col>
     </b-row>
     <b-row align-h="center" align-v="center" style="margin: 10%">
